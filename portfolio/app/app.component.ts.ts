c54278085d005842ec0cbd318d5af6ed49ecc15c
Vipinvarghesekn <42574500+Vipinvarghesekn@users.noreply.github.com>
2018-08-23 14:33:04 +0530

    Update app.component.ts.ts

--- portfolio/app/app.component.ts.ts
+++ portfolio/app/app.component.ts.ts
@@ -6,9 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  private title;
+  public title;
   setName() {
-    this.title = "go"; 
+    this.title = "hai";
   }
-
 }
